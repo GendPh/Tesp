c2a37083903d5e5cd6a90aac92fd79a524656388
Gabriel Ferreira <113285710+GendPh@users.noreply.github.com>
2023-10-20 21:32:19 +0100

    Add files via upload

---
+++ Linguagens de Programação I/C_Projetos/Person/Person/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace welcome
+{
+	internal class Person
+	{
+		public string name;
+		public int age;
+
+		public void Welcome(string nameInner, int ageInner)
+		{
+			Console.WriteLine($"Welcome {name} with {age} years old");
+            Console.WriteLine($"Welcome {nameInner} with {ageInner} years old");
+
+        }
+    }
+}
+
