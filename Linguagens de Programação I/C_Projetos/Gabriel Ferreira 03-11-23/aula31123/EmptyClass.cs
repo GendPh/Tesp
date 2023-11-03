3140c16206a15500bb52468918ff92d244497091
Gabriel Ferreira <113285710+GendPh@users.noreply.github.com>
2023-11-03 21:20:18 +0000

    Add files via upload

---
+++ Linguagens de Programação I/C_Projetos/Gabriel Ferreira 03-11-23/aula31123/EmptyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace aula31123
+{
+	public class Pessoa
+	{
+		
+		
+			public void comunicar(){
+				Console.WriteLine("Hello!");
+			}
+            public void comunicar(string name)
+            {
+                Console.WriteLine($"Hello {name}!");
+            }
+        
+	}
+}
+
---
+++ Linguagens de Programação I/C_Projetos/Gabriel Ferreira 03-11-23/aula31123/Exercicio2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace aula31123
+{
+	public class Exercicio2
+	{
+		private string? name;
+        private int saldo;
+
+		private int Deposito(int valor)
+		{
+			return valor + 500;
+		}
+
+		public void Mensagem()
+		{
+            Console.WriteLine("Qual é o seu nome: ");
+            name = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Inserir Saldo: ");
+			saldo = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine($"Olá {name} o seu novo saldo é de " +  Deposito(saldo));
+		}
+
+    }
+}
+
