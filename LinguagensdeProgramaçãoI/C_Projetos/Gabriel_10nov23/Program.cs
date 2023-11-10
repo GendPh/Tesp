698a8f9df96c8acdbcec94990e46b7ab6ab1d3eb
Gabriel Ferreira <113285710+GendPh@users.noreply.github.com>
2023-11-10 19:18:34 +0000

    criação da aula 10nov23

---
+++ LinguagensdeProgramaçãoI/C_Projetos/Gabriel_10nov23/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aula_10nov23
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      int Hours = 10;
+      int Minutes = 30;
+      int Seconds = 15;
+
+      Clock watch = new(Hours, Minutes, Seconds);
+      watch.SetTime(23, 23, 59);
+
+      int HourFromWatch = watch.GetHour();
+      System.Console.WriteLine($"Constructor Hour:{HourFromWatch}");
+
+      watch.ShowTime();
+    }
+  }
+}
---
+++ LinguagensdeProgramaçãoI/C_Projetos/Gabriel_10nov23/Relogio.cs
@@ -0,0 +1,45 @@
+﻿public class Clock
+{
+  public int hours;
+  public int minutes;
+  public int seconds;
+  public string period = "AM";
+
+
+  public Clock(int h, int m, int s)
+  {
+    hours = h;
+    minutes = m;
+    seconds = s;
+  }
+
+  public void SetTime(int h, int m, int s)
+  {
+    if (h > 0 && h <= 23)
+    {
+      hours = h;
+    }
+    if (m > 0 && m <= 59)
+    {
+      minutes = m;
+    }
+    if (s > 0 && s <= 59)
+    {
+      seconds = s;
+    }
+  }
+
+  public int GetHour()
+  {
+    return hours;
+  }
+
+  public void ShowTime()
+  {
+    if (hours >= 12)
+    {
+      period = "PM";
+    }
+    System.Console.WriteLine($"{hours}:{minutes}:{seconds} {period}");
+  }
+}
