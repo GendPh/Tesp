bdade588af27fade3570e667ed94bf4bce100b93
Gabriel Ferreira <113285710+GendPh@users.noreply.github.com>
2024-03-27 21:31:23 +0000

    save

--- Segundo_Sem/Programação_Web_II/Projects/Angular/my-app-27-03/src/app/app.component.ts
+++ Segundo_Sem/Programação_Web_II/Projects/Angular/my-app-27-03/src/app/app.component.ts
@@ -12,5 +12,19 @@ import { TurmaComponent } from './turma/turma.component';
 })
 
 export class AppComponent {
-  title = 'Aula 27/03/2024';
+  title: string = 'Aula 27/03/2024';
+  n: number = 0;
+
+  Add(n: number): void {
+    this.n += n;
+  }
+  Reduce(n: number): void {
+    this.n -= n;
+  }
+  Reset(): void {
+    this.n = 0;
+  }
+  ChangeTitle(e: Event): void {
+    this.title = (e.target as HTMLInputElement).value.length == 0 ? "Aula 27/03/2024" : (e.target as HTMLInputElement).value;
+  }
 }
