e91ae047566f16fdacc2b2868921cd6cd9ac4a60
Gabriel Ferreira <113285710+GendPh@users.noreply.github.com>
2024-04-03 20:35:00 +0100

    Update aula-030424 component with new functionality

--- Segundo_Sem/Programação_Web_II/Projects/Angular/my-app-27-03/src/app/aula-030424/aula-030424.component.ts
+++ Segundo_Sem/Programação_Web_II/Projects/Angular/my-app-27-03/src/app/aula-030424/aula-030424.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-aula-030424',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './aula-030424.component.html',
   styleUrl: './aula-030424.component.css'
 })
 export class Aula030424Component {
+  title: string = "Aula 03/04/24";
+  number1: number = 0;
+  number2: number = 0;
+  result1: number = 0;
+  number3: number = 0;
+  number4: number = 0;
+  result2: number = 0;
+  showP: boolean = false;
 
+  ShowP(): void {
+    this.showP = !this.showP;
+  }
+
+  Sum(): void {
+    this.result1 = this.number1 + this.number2;
+  }
+
+  AutoSum(): void {
+    this.result2 = this.number3 + this.number4;
+  }
+
+  ChangeTitle(e: Event): void {
+    this.title = (e.target as HTMLInputElement).value.length == 0 ? "Aula 03/04/24" : (e.target as HTMLInputElement).value;
+  }
 }
